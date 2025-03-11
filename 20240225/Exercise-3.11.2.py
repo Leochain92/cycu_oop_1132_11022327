d0ec18833cb798fdc6aa8d2d6b809d5e78da307a
Yu Cheng Lien11022327 <trucksun988@gmail.com>
2025-03-11 18:47:48 +0800

    Delete Exercise-3.11.2.py

--- 20240225/Exercise-3.11.2.py
+++
@@ -1,9 +0,0 @@
-def print_right(text):
-    total_width = 40
-    leading_spaces = total_width - len(text)
-    print(" " * leading_spaces + text)
-
-# 測試 print_right 函數
-print_right("Monty")
-print_right("Python's")
-print_right("Flying Circus")
