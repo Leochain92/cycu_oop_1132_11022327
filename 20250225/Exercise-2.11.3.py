031a5147985208bb190fbac6d88ecd6ccbf17eaf
Yu Cheng Lien11022327 <trucksun988@gmail.com>
2025-03-11 18:48:06 +0800

    Create Exercise-2.11.3.py

---
+++ 20250225/Exercise-2.11.3.py
@@ -0,0 +1,21 @@
+import math
+#part 1
+r=5
+volume = math.pow(5,3) * math.pi *4 /3
+print ('part1 volume=', volume)
+
+#part 2
+x =42
+v = math.sin(x) **2 + math. cos (x) **2
+print ('part2 =', v)
+
+#part 3
+import math
+print('math.e =' , math.e )
+result1 = math.e ** 2
+print(result1)
+result2 = math.pow(math.e, 2)
+print(result2)
+result3 = math.exp(2)
+print(result3)
+
